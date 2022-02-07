5f52d98bc2a8c0c0d71a467a7a5d46d614f06f50
Hector Nava <hector.r.nava44@gmail.com>
2022-02-07 09:06:47 -0700

    Setup server

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const sequelize = require('./config');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sessionSettings = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+};
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sessionSettings));
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+});
