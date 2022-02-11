39ae71d5ef9101467fe4c340e91cd2116d1ec94a
Hector Nava <hector.r.nava44@gmail.com>
2022-02-10 18:44:21 -0700

    Updated TODO comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,5 +1,7 @@
 const { Comment } = require('../models');
 
 module.exports = {
-    
+
+    // TODO: Add functions for comments (createComment, updateComment, deleteComment)
+
 };
--- controllers/heartController.js
+++ controllers/heartController.js
@@ -2,4 +2,6 @@ const { Heart } = require('../models');
 
 module.exports = {
 
+    // TODO: Add functions for hearts (createHeart, deleteHeart)
+
 };
