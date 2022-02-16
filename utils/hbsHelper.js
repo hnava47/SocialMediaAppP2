e6d93068269a7b866bccf6046ef346d1dc4cde08
Hector Nava <hector.r.nava44@gmail.com>
2022-02-15 18:32:20 -0700

    Updated logic to incorporate custom helper

--- utils/hbsHelper.js
+++ utils/hbsHelper.js
@@ -4,5 +4,13 @@ module.exports = {
     },
     minus: (a, b) => {
         return a - b;
+    },
+    inList: (a, list, key) => {
+        for (let i = 0; i < list.length; i++) {
+            if (a === list[i][key]) {
+                return list[i];
+            }
+        }
+        return;
     }
 };
