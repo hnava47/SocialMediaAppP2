11276417f15932dcd82826c42e4d8447529ac778
Cody Hicks <hickscj5@gmail.com>
2022-02-15 20:12:14 -0700

    fixed some comment files

--- routes/apiRoutes/commentRoutes/index.js
+++ routes/apiRoutes/commentRoutes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-
+const {createComment, deleteComment, updateComment} = require('../../../controllers/commentController') 
 // TODO: router.get('/:postId', viewComments)
 
  router.post('/', createComment);
- router.delete('/'. deleteComment);
+ router.delete('/', deleteComment);
  router.put('/:id', updateComment)
 
 // TODO: router.route('/:commentId')
