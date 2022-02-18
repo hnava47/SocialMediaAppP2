c2eaf9ab958383e5fee82fcfd676129971cb1c37
Hector Nava <hector.r.nava44@gmail.com>
2022-02-18 09:16:49 -0700

    Removed unused HBS helpers

--- utils/hbsHelper.js
+++ utils/hbsHelper.js
@@ -1,16 +1,5 @@
 module.exports = {
     equals: (a, b) => {
         return a === b;
-    },
-    minus: (a, b) => {
-        return a - b;
-    },
-    inList: (a, list, key) => {
-        for (let i = 0; i < list.length; i++) {
-            if (a === list[i][key]) {
-                return list[i];
-            }
-        }
-        return;
     }
 };
